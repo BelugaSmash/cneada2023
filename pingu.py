72e4965685b7025bcce1cc9940b636f12897eab1
htjung09 <htjung09@naver.com>
2023-06-17 14:31:26 +0900

    commit

--- pingu.py
+++ pingu.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+import random
 
 # 파이게임 초기화
 pygame.init()
@@ -21,9 +22,15 @@ player_w, player_h = 55, 55
 player_y = screen_h - floor_h - player_h
 opy = player_y
 gravity = 0 
-sliding = False
+sliding = False 
 jumping = False
 jumpCnt = 2
+
+# 장애물 변수 선언
+obs_x = [screen_w, screen_w * 3 / 2]
+obs_t = [random.randint(1, 2), random.randint(1, 2)]
+obs_w, obs_h = 55, 80
+obs_speed = 8
 
 # FPS 설정을 위한 변수
 clock = pygame.time.Clock()
@@ -46,7 +53,7 @@ while 1:
             if event.key == pygame.K_DOWN:
                 sliding = False
 
-
+    # 플레이어 움직이기
     player_y -= gravity
     if player_y >= opy:
         player_y = opy
@@ -54,13 +61,23 @@ while 1:
         jumpCnt = 2
     gravity -= 1.2
     
+    # 장애물 움직이기
+    for i in range(2): 
+        obs_x[i] -= obs_speed
+        if obs_x[i] + obs_w <= 0:
+            px = obs_x[abs(1 - i)] + screen_w / 2 + random.randint(0, 700)
+            obs_t[i] = random.randint(1, 2)
+            obs_x[i] += px
+    
     screen.fill((50, 150, 200))
     # 바닥 그리기
     pygame.draw.rect(screen, (100, 70, 70), [0, screen_h - floor_h, screen_w, floor_h])
     pygame.draw.rect(screen, (0, 200, 0), [0, screen_h - floor_h, screen_w, 30])
     # 플레이어 그리기
     pygame.draw.rect(screen, (0, 0, 255), [100, player_y + (player_h / 2 if sliding and not jumping else 0), player_w, player_h / (2 if sliding and not jumping  else 1)])
-
+    # 장애물 그리기
+    for i in range(2):
+        pygame.draw.rect(screen, (255, 0, 0), [obs_x[i], screen_h - floor_h - obs_h * obs_t[i], obs_w, obs_h * obs_t[i]])
     pygame.display.update()
 
 # 2초 기다리고 게임을 끈다.
