ceb5f633265bb8cc0ee0f923f30fca3e0a64da74
htjung09 <htjung09@naver.com>
2023-06-17 13:28:48 +0900

    asd

--- pingu.py
+++ pingu.py
@@ -16,7 +16,10 @@ screen = pygame.display.set_mode((screen_w, screen_h))
 cpath = os.path.dirname(__file__)
 #player_img = pygame.image.load("resources/pingu2.png")
 
-floor_height = 200
+floor_h = 200
+player_w, player_h = 55, 55
+player_y = screen_h - floor_h - player_h
+opy = player_y
 
 # FPS 설정을 위한 변수
 clock = pygame.time.Clock()
@@ -29,8 +32,9 @@ while 1:
             sys.exit()
     
     screen.fill((50, 150, 200))
-    pygame.draw.rect(screen, (100, 70, 70), [0, screen_h - floor_height, screen_w, floor_height])
-    pygame.draw.rect(screen, (0, 200, 0), [0, screen_h - floor_height, screen_w, 30])
+    pygame.draw.rect(screen, (100, 70, 70), [0, screen_h - floor_h, screen_w, floor_h])
+    pygame.draw.rect(screen, (0, 200, 0), [0, screen_h - floor_h, screen_w, 30])
+    pygame.draw.rect(screen, (0, 0, 255), [100, player_y, player_w, player_h])
     pygame.display.update()
 
 # 2초 기다리고 게임을 끈다.
