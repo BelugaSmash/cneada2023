285d9f1fa91d53dc6dc7f090e926987723255b45
htjung09 <htjung09@naver.com>
2023-07-08 15:19:41 +0900

    commit

--- pingu.py
+++ pingu.py
@@ -198,7 +198,7 @@ while 1:
         screen.blit(boss_hand[0], (boss_hand_x[0] + sc_shake_x, hand_y + sc_shake_y))
         screen.blit(boss_hand[1], (boss_hand_x[1] + sc_shake_x, hand_y + sc_shake_y))
     boss_rect = [boss_x + sc_shake_x, boss_y + sc_shake_y, 309, 800]
-    boss_hitbox = [boss_x + 30 + sc_shake_x, boss_y + 50 + sc_shake_y, 309 - 60, 750]
+    boss_hitbox = [boss_x + 50 + sc_shake_x, boss_y + 50 + sc_shake_y, 309 - 100, 750]
     if boss_attack == 0:
         screen.blit(boss_img, boss_rect)
     elif boss_attack == 1 or boss_attack == 2:
