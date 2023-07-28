c8fe45831620a75ea29c16fa078ad841a673572c
htjung09 <htjung09@naver.com>
2023-07-28 09:27:18 +0900

    COMMIT

--- pingu.py
+++ pingu.py
@@ -195,24 +195,20 @@ while 1:
                 # 아래 방향키 눌렀다면 슬라이딩 중으로 바꾸기
                 if event.key == pygame.K_s:
                     sliding = True
-                # 현재 중간보스이고, x키를 눌렀다면 총알 발사
-                if event.key == pygame.K_x and (mode == "m boss" or mode == "f boss") and attack_cool_frame <= 0:
-                    player_attack.append([player_x + 20, player_y + player_w / 2])
-                    attack_cool_frame = 10
-                if event.key == pygame.K_a:
-                    pressed_key.append("left")
-                if event.key == pygame.K_d:
-                    pressed_key.append("right")
+            if event.key == pygame.K_a:
+                pressed_key.append("left")
+            if event.key == pygame.K_d:
+                pressed_key.append("right")
         # 키를 뗀 경우
         if event.type == pygame.KEYUP:
             # 아래 방향키를 뗐다면 슬라이딩 중을 아님으로 바꾸기
             if not game_over:
                 if event.key == pygame.K_s:
                     sliding = False
-                if event.key == pygame.K_a:
-                    pressed_key.remove("left")
-                if event.key == pygame.K_d:
-                    pressed_key.remove("right")
+            if event.key == pygame.K_a:
+                pressed_key.remove("left")
+            if event.key == pygame.K_d:
+                pressed_key.remove("right")
 
     # 게임 오버가 아니라면
     if not game_over:
